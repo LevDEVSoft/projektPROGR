a8bce56930fcf0cf72a1a64e4c6796c60c2c788d
LeonardLume <144630224+LeonardLume@users.noreply.github.com>
2024-11-13 18:30:25 +0200

    Add files via upload

---
+++ Moodulid.py
@@ -0,0 +1,10 @@
+#Kood algab kõigi vajalike moodulite importimisega
+# Importime vajalikud moodulid
+
+import tkinter as tk  # graafiline kasutajaliide (GUI) loomiseks, et kuvada andmeid ja teateid.
+
+import matplotlib.pyplot as plt  #  graafiku loomine, et kuvada andmete trende (nt temperatuur ja valgus).
+
+import pandas as pd  # andmete töötlemine ja analüüsimiseks (nt energiakasutuse jälgimine).
+
+from tkinter import messagebox  # teadete  kuvamiseks GUI-s.
