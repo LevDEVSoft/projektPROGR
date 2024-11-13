4168266e2508cecdfe98efe62a4d3a716fbe0dc6
LeonardLume <144630224+LeonardLume@users.noreply.github.com>
2024-11-13 18:30:11 +0200

    Add files via upload

---
+++ Kommentaarid projekt.py
@@ -0,0 +1,11 @@
+################################################
+# Programmeerimine I
+# 2024/2025 sügissemester
+#
+# Projekt
+# Teema: Tark Energiasäästja
+#
+# Autorid: Leonard Lume, Lev Listov
+#
+# Projekt jälgib ja optimeerib energiakasutust
+##################################################
